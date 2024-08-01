f2582490f58b6dcab8b6c2520ee84a0612a29c06
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-08-02 02:14:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,3 @@
-# curl https://sh.rustup.rs -sSf | sh
-# sh -c "$(curl -sSfL https://release.solana.com/stable/install)"
-# source "$HOME/.cargo/env"
-# export PATH="/root/.local/share/solana/install/active_release/bin:$PATH"
-# solana-keygen new -o /root/.config/solana/id.json
-# solana config set --url d
-
-# sudo apt update
-# sudo apt install build-essential
-
-# # git clone https://github.com/regolith-labs/ore-cli.git
-# # cd ore-cli/
-# # cargo build --release
-# cargo install ore-cli@1.0.0-alpha.6 #将会安装测试网v2版本
-
-# export RUST_BACKTRACE=1
-# export RUST_BACKTRACE=full
-
-# # sudo ln -s /root/ore-cli/target/release/ore /usr/local/bin/ore
-# sudo ln -s /root/.cargo/bin/ore /usr/local/bin/ore
-# export PATH=$PATH:/usr/local/bin
-# source ~/.bashrc
-
 #要回车
 curl --proto '=https" --tlsv1.3 https://sh.rustup.rs -sSf | sh
 source "$HOME/.cargo/env"
@@ -43,3 +20,18 @@ git clone -b extended_stats --single-branch https://github.com/pmcochrane/ore-cl
 cd ore-cli/
 ./build_and_mine.sh 
 cp ore_env.priv.sh.sample ore_env.priv.sh
+chmod +x ore_env.priv.sh
+
+ORE_DIR="$HOME/extend-stats/ore-cli"
+ORE_FILE="$ORE_DIR/ore_env.priv.sh"
+read -p "请输入sol的rpc: " rpc
+rpc=${rpc:-"https://api.devnet.solana.com"}
+
+# 更新 solxen-tx.yaml 文件
+sed -i "s|KEY1=.*|KEY1=\~/.config/solana/id.json\|" $ORE_FILE
+sed -i "s|RPC1=.*|RPC1=$rpc|" $ORE_FILE
+
+./miner.sh 1
+
+
+
