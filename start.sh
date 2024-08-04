e473bf2342476068670eaa90a36a349b6f892456
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-08-04 22:07:38 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
-sudo apt update && sudo apt upgrade
-sudo apt-get install build-essential
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo apt install cargo
+echo "\$nrconf{kernelhints} = 0;" >> /etc/needrestart/needrestart.conf
+echo "\$nrconf{restart} = 'l';" >> /etc/needrestart/needrestart.conf
+sudo apt update -y
+sudo apt-get install build-essential -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path > /dev/null 2>&1
+. "$HOME/.cargo/env"
+sudo apt install cargo -y
 sh -c "$(curl -sSfL https://release.solana.com/stable/install)"
 export PATH="/root/.local/share/solana/install/active_release/bin:$PATH"
 solana-keygen new
