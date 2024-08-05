2be528c42b09ef99edb0bbe29db0b46d1c077755
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-08-05 14:44:40 +0800

    Update local_start.sh

--- local_start.sh
+++ local_start.sh
@@ -1,3 +1,5 @@
+sudo apt-get update
+sudo apt install git
 git clone https://github.com/hardhatchad/ore
 git clone https://github.com/hardhatchad/ore-cli
 git clone https://github.com/hardhatchad/drillx
@@ -5,6 +7,7 @@ cd ore && git checkout master && cd ..
 cd ore-cli && git checkout master && cd ..
 
 cd ore-cli
+export http=http://c38562:52488@47.90.169.195:16801
 cargo build --release
 cd target/release
 ./ore mine --keypair ~/.config/solana/id.json --threads 8 --rpc https://api.devnet.solana.com
