5e6a8bb99d65a747a7aa513a4f002aebb9f761cc
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-08-05 14:46:58 +0800

    Update local_start.sh

--- local_start.sh
+++ local_start.sh
@@ -10,4 +10,13 @@ cd ore-cli
 export http=http://c38562:52488@47.90.169.195:16801
 cargo build --release
 cd target/release
+
+
+export PATH="/root/ore-cli/target/release:$PATH"
+
+solana config set --url https://api.mainnet-beta.solana.com
+
+##测试hash
+ore benchmark --threads 4
+
 ./ore mine --keypair ~/.config/solana/id.json --threads 8 --rpc https://api.devnet.solana.com
